133e297c7222638419a6a14ee2f5901981c75c61
Rozumbunch <rozumbunch@gmail.com>
2026-09-14 14:41:15 +0200

    Update TYPO3 version constraint in ext_emconf.php to support TYPO3 12.4.0-14.3.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-14.0.99',
+            'typo3' => '12.4.0-14.3.99',
         ],
     ],
     'autoload' => [
